805e8a5be5ec1bcd3fd61cb3d2ec98ad20b6f44b
Febriaditiya <aditiya022002@gmail.com>
2022-05-28 13:41:30 +0700

    Data Barang

--- README.md
+++ README.md
@@ -47,3 +47,63 @@ koneksi berhasil, uncomment pada perintah echo “koneksi berhasil”;
 
 ![8.png](img/8.png)
 
+### Membuat File Index.PHP
+
+![9.png](img/9.png)
+
+![10.png](img/10.png)
+
+![11.png](img/11.png)
+
+### Membuat File tambah.PHP
+
+![12.png](img/12.png)
+
+![13.png](img/13.png)
+
+![14.png](img/14.png)
+
+### Membuat File ubah.PHP
+
+![15.png](img/15.png)
+
+![16.png](img/16.png)
+
+![17.png](img/17.png)
+
+![18.png](img/18.png)
+
+### Membuat File hapus.PHP
+
+![19.png](img/19.png)
+
+## Tampilan Data Barang
+
+![20.png](img/20.png)
+
+## Tampilan jika ingin menambahkan barang
+
+![21.png](img/21.png)
+
+*Hasil Barang yang di tambahkan*
+
+![22.png](img/22.png)
+
+## Tampilan Jika ingin Ubah Barang
+
+![23.png](img/23.png)
+
+*Hasil barang yang di ubah Pada tabel nomor 2*
+
+![24.png](img/24.png)
+
+## Tampilan Jika ingin menghapus barang 
+
+![25.png](img/25.png)
+
+*Barang yang sudah di hapus pada tabel nomor 3*
+
+![26.png](img/26.png)
+
+
+
--- index.php
+++ index.php
@@ -11,40 +11,59 @@ $result = mysqli_query($conn, $sql);
     <meta charset="UTF-8">
     <link href="style.css" rel="stylesheet" type="text/css" />
     <title>Data Barang</title>
+    <style>
+        .container {
+            background: #F0FFF0;
+        }
+        table {
+            background: #FAFAD2;
+        }
+        body {
+            background: #D2B48C;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <h1>Data Barang</h1>
+        <a href="tambah.php" class="tambah">Tambah Barang</a>
         <div class="main">
             <table>
             <tr>
+                <th>No</th>
                 <th>Gambar</th>
                 <th>Nama Barang</th>
-                <th>Katagori</th>
-                <th>Harga Jual</th>
+                <th>Kategori</th>
                 <th>Harga Beli</th>
+                <th>Harga Jual</th>
                 <th>Stok</th>
                 <th>Aksi</th>
             </tr>
             <?php if($result): ?>
-            <?php while($row = mysqli_fetch_array($result)): ?>
+            <?php $i = 1; ?>
+            <?php while($row = mysqli_fetch_array($result)):  ?>
             <tr>
-                <td><img src="gambar/<?= $row['gambar'];?>" alt="<?=
-                $row['nama'];?>"></td>
+                <td><?= $i; ?></td>
+                <td><img src="gambar/<?= $row['gambar'];?>" alt="<?= $row['nama'];?>" width="80">
+                </td>
                 <td><?= $row['nama'];?></td>
                 <td><?= $row['kategori'];?></td>
                 <td><?= $row['harga_beli'];?></td>
-                <td><?= $row['harga_jual'];?></td>
+                 <td><?= $row['harga_jual'];?></td>
                 <td><?= $row['stok'];?></td>
-                <td><?= $row['id_barang'];?></td>
+                <td>
+                <a class="ubah" href="ubah.php?id_barang=<?php echo $row['id_barang']; ?>">Ubah</a>
+                <a class="hapus" href="hapus.php?id_barang=<?php echo $row['id_barang']; ?>">Hapus</a>
+                </td>
             </tr>
+            <?php $i++; ?>
             <?php endwhile; else: ?>
             <tr>
                 <td colspan="7">Belum ada data</td>
             </tr>
             <?php endif; ?>
-            </table>
-        </div>
+      </table>
     </div>
+  </div>
 </body>
 </html>
